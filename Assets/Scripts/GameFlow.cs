69be0b09f309148a9480bebb4bc4419f54503c85
samet <sametykalan@gmail.com>
2022-06-23 11:14:19 +0300

    intial commit

---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameFlow : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    [SerializeField] State kritik;
+    [SerializeField] State kritikTr;
+    [SerializeField] State escapeCross;
+    [SerializeField] State escapeCrossTr;
+
+    State currentState;
+    void Start()
+    {
+        currentState = startingState;
+        textComponent.text = currentState.getStateText();
+        kritikTr.ayrim = true;
+        kritik.ayrim = true;
+        escapeCross.ayrim = false;
+        escapeCrossTr.ayrim = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageStates();
+    }
+
+    private void ManageStates()
+    {
+        var nextStates = currentState.getNextStates();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (currentState.nextStates.Length > 0)
+            {
+                if (currentState.name.Equals("Escape"))
+                {
+                    if (currentState.ayrim == true)
+                    {
+                        currentState = nextStates[1];
+                        return;
+                    }
+                    else
+                    {
+                        currentState = nextStates[0];
+                    }
+                }
+                else
+                {
+                    currentState = nextStates[0];
+                }
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (currentState.nextStates.Length > 1)
+            {
+                if (currentState.ayrim == true)
+                {
+                    escapeCross.ayrim = true;
+                    escapeCrossTr.ayrim = true;
+                }
+                currentState = nextStates[1];
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Q))
+            Application.Quit();
+
+        textComponent.text = currentState.getStateText();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,14)] [SerializeField] string storyText;
+    public State[] nextStates;
+    public bool ayrim = false;
+    public string getStateText()
+    {
+        return storyText;
+    }
+
+    public State[] getNextStates()
+    {
+        return nextStates;
+    }
+}
